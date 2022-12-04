3dec2ff9b3c77f3bc114d80b6109577c9d7217d0
Dejan Rakinic <dejanrakinic1304@gmail.com>
2022-12-04 16:33:04 +0100

    Add UI Test target for running high-level Acceptance Tests

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,10 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Dejan Rakinic on 4.12.22..
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTests: XCTestCase { }
